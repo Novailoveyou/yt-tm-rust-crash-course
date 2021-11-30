324b7318a6d0a39b7936fd9501acaf99d41f3932
novailoveyou <novailoveyoux@gmail.com>
2021-11-30 20:55:08 +0300

    vars & mutable vars

---
+++ src/vars.rs
@@ -0,0 +1,14 @@
+// Variables hold primitive data or references to data
+// Variables are immutable by default
+// Rust is a block-scoped language
+
+pub fn run() {
+  let name = "Nova";
+  let mut age = 24;
+
+  println!("My name is {} and I am {}", name, age);
+
+  age = 25;
+
+  println!("My name is {} and I am {}", name, age);
+}
