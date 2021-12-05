3a44db56a3a411d994b8d0251fc71a50e3995287
novailoveyou <novailoveyoux@gmail.com>
2021-12-05 14:42:27 +0300

    more primitive types: bool expresions & chars (char can be any unicode, so emojies would work too

--- src/types.rs
+++ src/types.rs
@@ -27,5 +27,11 @@ pub fn run() {
   // Boolean
   let is_active: bool = true;
 
-  println!("{:?}", (x, y, z, is_active));
+  // Get boolean from expression
+  let is_greater: bool = 10 > 5;
+
+  let a1 = 'a';
+  let face = '\u{1F600}';
+
+  println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
 }
