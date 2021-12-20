37c419e8362bfbba369923f8e23680005da7d26f
novailoveyou <novailoveyoux@gmail.com>
2021-12-20 03:35:04 +0300

    arrays: mutate single value

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,10 +1,13 @@
 // Arrays - Fixed list where elements are the same data types
 
 pub fn run() {
-  let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+  let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+  // Re-assign value
+  numbers[2] = 20;
 
   println!("{:?}", numbers);
 
   // Get single val
-  println!("Single Value: {}", numbers[0])
+  println!("Single Value: {}", numbers[0]);
 }
