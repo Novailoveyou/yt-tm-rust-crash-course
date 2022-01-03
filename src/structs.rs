00ebeb573f14a05633301c26c9e25f009cd8a6eb
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 16:43:44 +0300

    mutatable self prop

--- src/structs.rs
+++ src/structs.rs
@@ -28,6 +28,11 @@ impl Person {
   fn full_name(&self) -> String {
     format!("{} {}", self.first_name, self.last_name)
   }
+
+  // Set last name
+  fn set_last_name(&mut self, last: &str) {
+    self.last_name = last.to_string();
+  }
 }
 
 pub fn run() {
@@ -47,7 +52,8 @@ pub fn run() {
 
   // println!("Color: {} {} {}", c.0, c.1, c.2);
 
-  let mut p = Person::new("John", "Doe");
+  let mut p = Person::new("Mary", "Doe");
   println!("Person {}", p.full_name());
-  println!("Person {} {}", p.first_name, p.last_name);
+  p.set_last_name("Williams");
+  println!("Person {}", p.full_name());
 }
