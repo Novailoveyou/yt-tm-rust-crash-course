6c6cff377be2b90052a6c414cd75147a43b7a798
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 16:11:09 +0300

    tuple structs

--- src/structs.rs
+++ src/structs.rs
@@ -1,20 +1,29 @@
 // Structs - Used to create custom data types
 
-// Traditional Struct
-struct Color {
-  red: u8,
-  green: u8,
-  blue: u8,
-}
+// // Traditional Struct
+// struct Color {
+//   red: u8,
+//   green: u8,
+//   blue: u8,
+// }
+
+// Tuple Struct
+struct Color(u8, u8, u8);
 
 pub fn run() {
-  let mut c = Color {
-    red: 255,
-    green: 0,
-    blue: 0,
-  };
+  // let mut c = Color {
+  //   red: 255,
+  //   green: 0,
+  //   blue: 0,
+  // };
 
-  c.red = 200;
+  // c.red = 200;
 
-  println!("Color: {} {} {}", c.red, c.green, c.blue);
+  // println!("Color: {} {} {}", c.red, c.green, c.blue);
+
+  let mut c = Color(255, 0, 0);
+
+  c.0 = 200;
+
+  println!("Color: {} {} {}", c.0, c.1, c.2);
 }
