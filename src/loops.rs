be9e79f5dfd17f8ddee47830cab041e2a7210947
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 15:48:42 +0300

    while loop

--- src/loops.rs
+++ src/loops.rs
@@ -3,13 +3,28 @@
 pub fn run() {
   let mut count = 0;
 
-  // Infinite loop
-  loop {
-    count += 1;
-    println!("Number: {}", count);
+  // // Infinite loop
+  // loop {
+  //   count += 1;
+  //   println!("Number: {}", count);
 
-    if count == 20 {
-      break;
+  //   if count == 20 {
+  //     break;
+  //   }
+  // }
+
+  // While Loop (FizzBuzz)
+  while count <= 100 {
+    if count % 15 == 0 {
+      println!("fizzbuzz");
+    } else if count % 3 == 0 {
+      println!("fizz");
+    } else if count % 5 == 0 {
+      println!("buzz");
+    } else {
+      println!("{}", count);
     }
+    // Inc
+    count += 1;
   }
 }
