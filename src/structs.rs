5ff9463ce737b314afb1d155b4a2502a53e5ec9a
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 16:08:21 +0300

    structs & traditional structs, kinda like objects in javascript

---
+++ src/structs.rs
@@ -0,0 +1,20 @@
+// Structs - Used to create custom data types
+
+// Traditional Struct
+struct Color {
+  red: u8,
+  green: u8,
+  blue: u8,
+}
+
+pub fn run() {
+  let mut c = Color {
+    red: 255,
+    green: 0,
+    blue: 0,
+  };
+
+  c.red = 200;
+
+  println!("Color: {} {} {}", c.red, c.green, c.blue);
+}
