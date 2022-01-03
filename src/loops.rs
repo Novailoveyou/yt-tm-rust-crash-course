747f0c15e20140d43b15edf07bd3d485feca0bd6
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 15:49:29 +0300

    for range loop

--- src/loops.rs
+++ src/loops.rs
@@ -13,18 +13,31 @@ pub fn run() {
   //   }
   // }
 
-  // While Loop (FizzBuzz)
-  while count <= 100 {
-    if count % 15 == 0 {
+  // // While Loop (FizzBuzz)
+  // while count <= 100 {
+  //   if count % 15 == 0 {
+  //     println!("fizzbuzz");
+  //   } else if count % 3 == 0 {
+  //     println!("fizz");
+  //   } else if count % 5 == 0 {
+  //     println!("buzz");
+  //   } else {
+  //     println!("{}", count);
+  //   }
+  //   // Inc
+  //   count += 1;
+  // }
+
+  // For Range
+  for x in 0..100 {
+    if x % 15 == 0 {
       println!("fizzbuzz");
-    } else if count % 3 == 0 {
+    } else if x % 3 == 0 {
       println!("fizz");
-    } else if count % 5 == 0 {
+    } else if x % 5 == 0 {
       println!("buzz");
     } else {
-      println!("{}", count);
+      println!("{}", x);
     }
-    // Inc
-    count += 1;
   }
 }
