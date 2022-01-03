a4f54d4ef9b685ea172334917eebd3c0b339df73
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 15:55:23 +0300

    returning from a function

--- src/functions.rs
+++ src/functions.rs
@@ -2,8 +2,17 @@
 
 pub fn run() {
   greeting("Hello", "Jane");
+
+  // Bind function values to variables
+  let get_sum = add(5, 5);
+  println!("Sum: {}", get_sum);
 }
 
 fn greeting(greet: &str, name: &str) {
   println!("{} {}, nice to meet you!", greet, name);
+}
+
+// To return something simply don't use semicolon
+fn add(n1: i32, n2: i32) -> i32 {
+  n1 + n2
 }
